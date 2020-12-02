cc7ae473cfb128cb66833d3d539e15bf75fca6f2
Shinnosuke Iino <sengoku731sin@gmail.com>
2020-12-02 14:22:47 +0900

    入力された値を保存したファイルを読み込み，フォームの下に表示

--- public_html/kadai/kadai2/index.php
+++ public_html/kadai/kadai2/index.php
@@ -13,9 +13,14 @@ if (isset($_GET["name"]) && isset($_GET["comment"])) {
     $last_line = $line;
   }
 
-  $last_elements = explode("<>", $last_line);
-  $last_index = $last_elements[0];
-  $index = $last_index + 1;//最後の行のindex+1
+  $last_elements = [];
+  $index = 1;
+
+  if (isset($last_line)) {//ファイルが空白ではない時，最後の行のindexを参照する
+    $last_elements = explode("<>", $last_line);
+    $last_index = $last_elements[0];
+    $index = $last_index + 1;//最後の行のindex+1
+  }
   
   $text_mergred = $index . "<>" . $name . "<>" . $comment . "<>" . $time;
   fwrite($fp, $text_mergred . PHP_EOL);
@@ -40,11 +45,25 @@ if (isset($_GET["name"]) && isset($_GET["comment"])) {
         <p>名前：</p>
         <input type="text" name="name">
       </div>
-      <div class="form-element">
+      <div class="form-element set_btn">
         <p>コメント：</p>
         <input type="text" name="comment">
+        <button class="btn-submit" type="submit">送信</button>
       </div>
-      <button class="btn-submit" type="submit">送信</button>
+      <div class="comment_lineup">
+        <?php 
+          $fp = fopen("input_data.txt", "r");
+          while ($line = fgets($fp)) {
+            $line_elements = explode("<>", $line);
+            echo "<p>";
+            foreach ($line_elements as $ele) {
+              echo $ele . " ";
+            }
+            echo "</p>";
+          }
+          fclose($fp);
+        ?>
+      </div>
     </form>
   
   </div>
